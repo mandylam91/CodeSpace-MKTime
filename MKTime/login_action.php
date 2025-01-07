779ef0bb2120e40af61b8ba8bd94fd983bb78e66
mandylam91 <54041104+mandylam91@users.noreply.github.com>
2025-01-07 10:09:52 +0000

    amended file opath to connects file

--- MKTime/login_action.php
+++ MKTime/login_action.php
@@ -1,6 +1,6 @@
 <?php
 if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' )
-require ( 'connect_db.php' ) ;
+require ( 'includes/connect_db.php' ) ;
 require ( 'login_tools.php' ) ;
 list ( $check, $data ) = validate ( $link, $_POST[ 'email' ], $_POST[ 'pass' ] ) ;
 if ( $check )
